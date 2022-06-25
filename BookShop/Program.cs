4632e78f3766e07a5ea8a5590559c7736a9d2324
sagit <minhquoc1423@gmail.com>
2022-06-25 20:47:27 +0700

    create loop

--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -6,17 +6,35 @@ namespace BookShop
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter ID:     ");
-            int id = int.Parse(Console.ReadLine());
-            Console.Write("Enter title:  ");
-            string title = Console.ReadLine();
-            Console.Write("Enter author: ");
-            string author = Console.ReadLine();
-            Console.Write("Enter price:  ");
-            decimal price = decimal.Parse(Console.ReadLine());
             IBook shop = new Shop();
-            shop.AddBook(id, title, author, price);
-            Console.WriteLine(shop.ShowAllBooksInfo());
+            int input = 9;
+            while (input != 0)
+            {
+                Console.WriteLine("1. Add book");
+                Console.WriteLine("2. Show information of all books");
+                input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Console.Write("Enter ID:     ");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Write("Enter title:  ");
+                        string title = Console.ReadLine();
+                        Console.Write("Enter author: ");
+                        string author = Console.ReadLine();
+                        Console.Write("Enter price:  ");
+                        decimal price = decimal.Parse(Console.ReadLine());
+                        shop.AddBook(id, title, author, price);
+                        break;
+                    case 2:
+                        Console.WriteLine(shop.ShowAllBooksInfo());
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }

8d25f9d4d87ee8d6f10f7a0db4ac1e59ae907212
sagit <minhquoc1423@gmail.com>
2022-06-25 19:53:13 +0700

    Add interface  IBook

---
+++ BookShop/IBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BookShop
+{
+    public interface IBook
+    {
+        void AddBook();
+        string ShowAllBooksInfo();
+        Book ShowBookById(int id);
+        bool RemoveBookById(int id);
+        bool UpdateBookById(int id, string title, string author, decimal price);
+    }
+}
+
---
+++ BookShop/Shop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BookShop
+{
+    public class Shop
+    {
+        public Shop()
+        {
+        }
+    }
+}
+
